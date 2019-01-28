1e753c124f8c1acb373f5326f30792a4be4542e8
Davluk <Davluka_D@hotmail.com>
2019-01-28 14:28:55 -0600

    Update GeneticAlgorithm.lua
    more information above the genetic algorithm in general

--- lua_GA/GeneticAlgorithm.lua
+++ lua_GA/GeneticAlgorithm.lua
@@ -1,9 +1,24 @@
 GenAl={}
 
-GenAl.newGenAl=funciton()
+GenAl.newGenAl=funciton(chromosomeSize, populationSize, populationInit, selection, crossover, mutation, crossChance, mutationChance)
+    self.population = populationInit(chromosomeSize,populationSize)
+    self.newpopulation = {}
+    self.getNewPop = populationInit
+    self.selection = selection
+    self.mutation = mutation
+    self.crossover = crossover
+    self.crossChance = crossChance
+    self.mutationChance = mutationChance
 end
 
-GenAl.Evolve=funciton()
+GenAl.Evolve=funciton(maxIterations)
+    counter=0
+    while(counter<maxIterations)
+        self.newpopulation=self.selection()
+        self.newpopulation=self.crossover(self.crossChance)
+        self.population=self.mutation(self.mutationChance)
+        counter=counter+1
+    end
 end
 
 return GenAl
