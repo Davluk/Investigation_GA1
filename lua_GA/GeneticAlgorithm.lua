89383de75933b7dc3e9e4ef554ba6a8590734925
Davluk <Davluka_D@hotmail.com>
2019-01-29 09:59:31 -0600

    Update GeneticAlgorithm.lua
    finish the genetic algorithm scheme

--- lua_GA/GeneticAlgorithm.lua
+++ lua_GA/GeneticAlgorithm.lua
@@ -1,24 +1,27 @@
 GenAl={}
 
-GenAl.newGenAl=funciton(chromosomeSize, populationSize, populationInit, selection, crossover, mutation, crossChance, mutationChance)
-    self.population = populationInit(chromosomeSize,populationSize)
-    self.newpopulation = {}
-    self.getNewPop = populationInit
-    self.selection = selection
-    self.mutation = mutation
-    self.crossover = crossover
-    self.crossChance = crossChance
+GenAl.newGenAl=funciton(chromosomeSize, populationSize, populationInit,fitness, selection, crossover, mutation, crossChance, mutationChance)
+    self.chromosomeSize = chromosomeSize
+    self.populationSize = populationSize
+    self.NewPop         = populationInit
+    self.fitness        = fitness
+    self.selection      = selection
+    self.crossover      = crossover
+    self.mutation       = mutation
+    self.crossChance    = crossChance
     self.mutationChance = mutationChance
 end
 
 GenAl.Evolve=funciton(maxIterations)
     counter=0
+    self.population = self.NewPop(self.chromosomeSize,self.populationSize,self.fitness)
     while(counter<maxIterations)
-        self.newpopulation=self.selection()
-        self.newpopulation=self.crossover(self.crossChance)
-        self.population=self.mutation(self.mutationChance)
+        self.newpopulation  =self.selection(self.population)
+        self.newpopulation  =self.crossover(self.newpopulation,self.crossChance)
+        self.population     =self.mutation(self.newpopulation,self.mutationChance)
         counter=counter+1
     end
+    return self.population
 end
 
 return GenAl
