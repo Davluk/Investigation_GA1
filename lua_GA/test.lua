dc9ea0dbe2cb0eb49d95ca93eab45b48c14f688d
Davluk <Davluka_D@hotmail.com>
2019-01-30 09:14:25 -0600

    test file

--- lua_GA/test.lua
+++ lua_GA/test.lua
@@ -1,1 +1,12 @@
-print("hola mundo")
+local GA= require("GeneticAlgorithm")
+
+local sizeOfchromosome = 9
+local sizeOfpopulation = 50
+local crossoverProbability = 50/100
+local mutationProbability = 10/100
+
+function newChromosome(size)
+    
+end
+
+function newPopulation(sizeP,sizeC)
