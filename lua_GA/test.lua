bd2d82b09e3570814c63c7b961d967b863d3d87a
Davluk <Davluka_D@hotmail.com>
2019-02-07 09:53:30 -0600

    Update test.lua

--- lua_GA/test.lua
+++ lua_GA/test.lua
@@ -4,4 +4,6 @@ inspect=require('inspect')
 
 math.randomseed(os.time())
 local mytree = tr.newTree(10)
-tr.print_preorder(mytree)
+tr.print_inorder(mytree)
+print('\n'..tr.countNodes(mytree))
+print('\n result: '..tr.EvalExpression(mytree))
