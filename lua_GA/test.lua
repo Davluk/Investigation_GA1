ae94b63ccb170fcbae6a6df6396c62001ffe0266
Davluk <Davluka_D@hotmail.com>
2019-02-11 11:05:10 -0600

    Update test.lua

--- lua_GA/test.lua
+++ lua_GA/test.lua
@@ -1,9 +1,17 @@
 math=require('math')
-tr = require('tree')
+tr = require('tree2')
 inspect=require('inspect')
 
+variables = {'x'}
+operators={'+','-','*','/'}
+terminals={1,2,3,4,5,6,7,8,9}
+
 math.randomseed(os.time())
-local mytree = tr.newTree(10)
-tr.print_inorder(mytree)
+tr:initTreeManager(operators,terminals,variables)
+local mytree = tr:NewTree(10)
+tr:printInOrder(mytree.data)
+--print(inspect(mytree.data))
+--[[tr.print_inorder(mytree)
 print('\n'..tr.countNodes(mytree))
 print('\n result: '..tr.EvalExpression(mytree))
+]]
