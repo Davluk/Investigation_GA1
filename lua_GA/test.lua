1a5900f91bc5dddad7c7269cd43741868c02a4aa
Davluk <Davluka_D@hotmail.com>
2019-02-21 11:18:18 -0600

    Update test.lua

--- lua_GA/test.lua
+++ lua_GA/test.lua
@@ -5,7 +5,7 @@ inspect=require('inspect')
 variables = {'x'}
 variable_values = {{x=2,y=4}}
 operators={'+','-','*','/'}
-terminals={1,2,3,4,5,6,7,8,9}
+terminals={1,2,5,7,9}
 
 function add(a,b)return a+b end
 function sub(a,b)return a-b end
@@ -18,10 +18,10 @@ operator_functions[operators[2]]=sub
 operator_functions[operators[3]]=mul
 operator_functions[operators[4]]=div
 
-math.randomseed(os.time())
+--math.randomseed(os.time())
 tr:initTreeManager(operators,terminals,variables,operator_functions)
-local mytree = tr:NewTree(5,variable_values,'y')
-tr:printInOrder(mytree.data)
+local mytree = tr:NewTree(10,variable_values,'y')
+tr:printPosOrder(mytree.data)
 mytree.fittness = tr:getCuadraticError(mytree.data,variable_values,'y')
 io.write('\n')
 print(tr:EvaluateFunction(mytree.data,variable_values[1]))
@@ -33,7 +33,7 @@ op = math.ceil( op/2 )
 print(op)
 
 mytree2 = tr:returnIndexedSubTree(mytree.data,op)
-tr:printInOrder(mytree2.data)
+--tr:printInOrder(mytree2.data)
 
 --print(inspect(mytree.data))
 --[[tr.print_inorder(mytree)
