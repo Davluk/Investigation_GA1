89e323a4921a28c69fc61de10ea88415f7dbf1e9
Davluk <Davluka_D@hotmail.com>
2019-02-25 14:57:01 -0600

    Update test.cpp

--- Cpp_GA/test.cpp
+++ Cpp_GA/test.cpp
@@ -2,20 +2,33 @@
 #include<stdlib.h>
 #include<time.h>
 
+
+
 enum varCode{ x, y };
-enum opCode{ add,sub,mul,div };
+enum opCode{ add,sub,mul,divp };
 int variables[]={x,y};
+int operadores[]={add,sub,mul,divp};
+
+char repOperators[] = {'+','-','*','/'};
+char rePVariables[] = {'x','y'};
 
 float add_func(float a,float b){ return a+b; }
 float sub_func(float a,float b){ return a-b; }
 float mul_func(float a,float b){ return a*b; }
 float div_func(float a,float b){ return (b==0)?(a/b):(a/0.001); } //divition protected
 
+
+
 int main(int argc, char const *argv[])
 {
     srand ((int)time(NULL));
-    float (*operations[])(float,float)={add_func,sub_func,mul_func,div_func};
-    
+    float (*operations[])(float,float)={};
+    operations[add]=add_func;
+    operations[sub]=sub_func;
+    operations[mul]=mul_func;
+    operations[divp]=div_func;
+
+
 
     return 0;
 }
