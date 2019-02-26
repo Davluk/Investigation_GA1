7eb077b847cddff79013352a253695273d5af72b
Davluka <Davluka_D@hotmail.com>
2019-02-26 11:39:00 -0600

    Update test.cpp

--- Cpp_GA/test.cpp
+++ Cpp_GA/test.cpp
@@ -15,18 +15,23 @@ char rePVariables[] = {'x','y'};
 float add_func(float a,float b){ return a+b; }
 float sub_func(float a,float b){ return a-b; }
 float mul_func(float a,float b){ return a*b; }
-float div_func(float a,float b){ return (b==0)?(a/b):(a/0.001); } //divition protected
+float div_func(float a,float b){ return (b!=0)?(a/b):(a/0.001); } //divition protected
 
-
+void executeFunctions(float a,float b,float (*_multi)(float,float),float (*_divi)(float,float)){
+    printf("the result of multiply a,b is %f\n",_multi(a,b));
+    printf("the result of divide a,b is %f\n",_divi(a,b));
+}
 
 int main(int argc, char const *argv[])
 {
     srand ((int)time(NULL));
-    float (*operations[])(float,float)={};
+    float (*operations[4])(float,float)={};
     operations[add]=add_func;
     operations[sub]=sub_func;
     operations[mul]=mul_func;
     operations[divp]=div_func;
+
+    executeFunctions(10,4,mul_func,div_func);
 
 
 
