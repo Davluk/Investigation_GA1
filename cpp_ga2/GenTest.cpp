e5090e07cf01179e062f405517ee3bf3b7960466
Davluka <Davluka_D@hotmail.com>
2019-03-28 19:52:12 -0600

    Update GenTest.cpp

--- cpp_ga2/GenTest.cpp
+++ cpp_ga2/GenTest.cpp
@@ -3,25 +3,28 @@
 #include <time.h>
 #include "GeneticProgramming.cpp"
 
-const int max_g = 100;
-const int pop_size = 100;
-const int max_depth = 3;
-const int fill_opt = HALF_HALF;
+const int max_g = 10000;
+const int pop_size = 1000;
+const int max_depth = 5;
+
+const double m_rate = 0.5f;
+const double c_rate = 0.90f;
+const double cross_prop = 0.7f;
+
+const int fill_opt = RAND_FILL;
 const int m_opt = BIT_MUT;
 const int c_opt = RAND_CROSS;
-const int s_opt = ROULT;
-const float m_rate = 0.05f;
-const float c_rate = 0.90f;
+const int s_opt = TOURN;
 
 const int TER_SIZ =9;
 const int OP_SIZ  =4;
 const int VAR_SIZ =2;
 
 enum nodeTypes{_var,_op,_term};
-float _values[][VAR_SIZ]={{0.0f,0.0f},{1.0f,1.0f},{2.0f,4.0f},{3.0f,9.0f},{4.0f,16.0f},{5.0f,25.0f},
-						   {6.0f,36.0f},{7.0f,49.0f},{8.0f,64.0f},{9.0f,81.0f},{10.0f,100.0f},
-						   {11.0f,121.0f},{12.0f,144.0f},{13.0f,169.0f},{14.0f,196.0f},{15.0f,225.0f},
-						   {16.0f,256.0f},{17.0f,289.0f},{18.0f,324.0f},{19.0f,361.0f},{20.0f,400.0f}};
+double _values[20][VAR_SIZ];
+double func(double x){return 9*x*x*x + 2*x*x + x + 4;}
+//*/
+
 char terRep[TER_SIZ]= {'1','2','3','4','5','6','7','8','9'};
 char opRep[OP_SIZ] = {'+','-','*','/'};
 char varRep[VAR_SIZ]= {'x','y'};//the last element is the F(x)
@@ -35,7 +38,7 @@ mD GETL()
 {
 	mD tempMD;
 	int coin = rand()%4;
-	if(coin>2)
+	if(coin>=2)
 	{ tempMD.data = rand()%sizeof(terRep)/sizeof(terRep[0]); tempMD.type=_term; }
 	else
 	{ tempMD.data = rand()%((sizeof(varRep)/sizeof(varRep[0]))-1); tempMD.type=_var; }
@@ -64,18 +67,18 @@ bool IV(mD _metadata){ return _metadata.type==_var; }
 
 int   GVI(mD _metadata){return _metadata.data;}
 int   GEI(mD _metadata){return _metadata.data;}
-float GT(mD _metadata){return (float)_metadata.data+1;}
+double GT(mD _metadata){return (double)_metadata.data+1;}
 
 // para cuando la funcion es un arbol de desiciones | clasificador
 int INTERPRETER(mD _metadata){return _metadata.data;}
 
 /* conjunto de funciones que evaluan la expresion */
-float ADD(float a,float b){return a+b;}
-float SUB(float a,float b){return a-b;}
-float MUL(float a,float b){return a*b;}
-float DIV(float a,float b){return (b==0)?(a/0.000001f):(a/b);}
+double ADD(double a,double b){return a+b;}
+double SUB(double a,double b){return a-b;}
+double MUL(double a,double b){return a*b;}
+double DIV(double a,double b){return (b==0)?(a/0.000000000001f):(a/b);}
 
-float EvEx(int selection,float a,float b)
+double EvEx(int selection,double a,double b)
 {
 	switch(selection){
 		case 0: return ADD(a,b);
@@ -91,23 +94,25 @@ float EvEx(int selection,float a,float b)
 
 int main(int argc, char const *argv[])
 {
-	//srand(time(NULL));
-	GenAlg<mD,float>* mygenalg = newGA(fill_opt,m_opt,c_opt,s_opt,pop_size,max_depth,max_g,m_rate,c_rate,0.7f,EvEx,GT,GETO,GETL,IN,IV,GVI,GEI,GCR);
+	srand(time(NULL));
+	for(int index=0;index<20;index++)
+	{
+		_values[index][0]=(double)(index/(double)1);
+		_values[index][1]=func(_values[index][0]);
+	}
+	GenAlg<mD,double>* mygenalg = newGA(fill_opt,m_opt,c_opt,s_opt,pop_size,max_depth,max_g,m_rate,c_rate,cross_prop,EvEx,GT,GETO,GETL,IN,IV,GVI,GEI,GCR);
+	
 	printf("INIT POBLATION \n\n");
 	initPobRec(mygenalg,_values,(size_t)sizeof(_values)/sizeof(_values[0]),0);
-	printPobStatus(mygenalg);
+
+	printf("#############################################\n####################POB#########################\n");
+	printBestIndiv(mygenalg);
 
 	GENETICPROSSES(mygenalg,_values,(size_t)sizeof(_values)/sizeof(_values[0]));
 
-	//printf("#############################################\n####################POB#########################\n");
-	//SELECTION(mygenalg);
+	printf("#############################################\n####################POB#########################\n");
+	printBestIndiv(mygenalg);
 	//printPobStatus(mygenalg);
-	//printf("#############################################\n####################POB#########################\n");
-	//CROSSOVER(mygenalg,_values,(size_t)sizeof(_values)/sizeof(_values[0]));
-	//printPobStatus(mygenalg);
-	//printf("#############################################\n####################POB#########################\n");
-	//MUTATION(mygenalg,_values,(size_t)sizeof(_values)/sizeof(_values[0]));
-	printPobStatus(mygenalg);
 
 	std::cout<< "holamundo";
 	return 0;
