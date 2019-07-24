a87a876216654d298e3780908085f2f4c650f8a3
DavlukaAgMo <Davluka_D@hotmail.com>
2019-07-23 23:05:23 -0500

    Update NodeTest.cpp

--- cpp_ga2/NodeTest.cpp
+++ cpp_ga2/NodeTest.cpp
@@ -5,48 +5,54 @@
 #include<exception>
 #include<iostream>
 #include<string>
+#include<vector>
 #include"Node.cpp"
 
-const int VARIABLE_SIZE = 2;
-const int DEPTH = 3;
 
 enum nodeTypes{_unop,_binop,_var,_term};													
-float** _values;
-std::basic_string<char> terRep[] = {"1","2","3","4","5","6","7","8","9","sqt(2)","sqt(3)","pi","e"};
-std::basic_string<char> binOpRep[] = {"+","-","*","/","^"};
-std::basic_string<char> unOpRep[]  = {"exp","ln","sin","cos"};
-std::basic_string<char> varRep[] = { "x","y"};//the last element is the F(x)												
+std::vector<double*> _values;
+//std::vector<std::string> terRep 	{"1","2","3","4","5","6","7","8","9"};
+std::vector<std::string> terRep 	{"1","2","3","4","5","6","7","8","9","sqrt(2)","sqrt(3)","pi","e"};
+std::vector<std::string> binOpRep	{"+","-","*","/"};
+//std::vector<std::string> binOpRep {"+","-","*","/","^"};
+std::vector<std::string> unOpRep	{"exp","ln","sin","cos"};
+std::vector<std::string> varRep		{ "x","y"};//the last element is the F(x)												
 
-float f(float x){ return 9*x*x*x + 4*x*x; }
+double f(double x){ return x*x; }												
 
-typedef struct METADATA{ 
+const int DEPTH = 3;
+const int VARIABLE_SIZE = 2;
+const float PROP = 0.55;
+const int samples =10;
+
+typedef struct mD{ 
 	int data; int type;
-	METADATA(){ data = 0; type = 0; }
+	mD(){ data = 0; type = 0; }
 } metaD;
 
-metaD GETLEAF()
+metaD GETL()
 {
 	metaD tempMD;
 	int coin = rand()%4;
 	if(coin>2)
-	{ tempMD.data = rand()%sizeof(terRep)/sizeof(terRep[0]); tempMD.type=_term; }
+	{ tempMD.data = rand()%terRep.size(); tempMD.type=_term; }
 	else
-	{ tempMD.data = rand()%((sizeof(varRep)/sizeof(varRep[0]))-1); tempMD.type=_var; }
+	{ tempMD.data = rand()%(varRep.size()-1); tempMD.type=_var; }
 	return tempMD;
 }
 
-metaD GETOP(){ 
+metaD GETO(){ 
 	metaD tempMD;
 	int coin = rand()%4;
-	if(coin>2){
-		tempMD.data = rand()%sizeof(unOpRep)/sizeof(unOpRep[0]); tempMD.type = _unop;
+	if(coin>1){//coin>=0
+		tempMD.data = rand()%binOpRep.size(); tempMD.type = _binop;
 	}else{
-		tempMD.data = rand()%sizeof(binOpRep)/sizeof(binOpRep[0]); tempMD.type = _binop;
+		tempMD.data = rand()%unOpRep.size(); tempMD.type = _unop;
 	}
 	return tempMD;
 }
 
-std::basic_string<char> GETCHARREP(metaD _metadata){
+std::string GCHR(metaD _metadata){
 	switch(_metadata.type)
 	{
 		case _binop:return binOpRep[_metadata.data]; break;
@@ -58,14 +64,14 @@ std::basic_string<char> GETCHARREP(metaD _metadata){
 	return ".";
 }
 //para cuando sea una funcion
-bool ISNODE(metaD _metadata){ return (_metadata.type==_unop||_metadata.type==_binop); }
-bool ISBIN (metaD _metadata){ return _metadata.type==_binop; }
-bool ISVAR (metaD _metadata){ return _metadata.type==_var; }
+bool IND (metaD _metadata){ return (_metadata.type==_unop||_metadata.type==_binop); }
+bool IBN (metaD _metadata){ return _metadata.type==_binop; }
+bool IVR (metaD _metadata){ return _metadata.type==_var; }
 
-int   GETVARINDEX(metaD _metadata){ return _metadata.data; }
-int   GETEXPINDEX(metaD _metadata){ return _metadata.data; }
-float GETTERMINAL(metaD _metadata){ 
-	return _metadata.data <9  ? (float)_metadata.data+1 :
+int   GVI(metaD _metadata){ return _metadata.data; }
+int   GEI(metaD _metadata){ return _metadata.data; }
+double GT(metaD _metadata){ 
+	return _metadata.data <9  ? (double)_metadata.data+1 :
 		   _metadata.data==9  ? sqrt(2) :
 		   _metadata.data==10 ? sqrt(3) :
 		   _metadata.data==11 ? 3.14159265358979323846f : exp(1) ;
@@ -74,19 +80,19 @@ float GETTERMINAL(metaD _metadata){
 // para cuando la funcion es un arbol de desiciones | clasificador
 int INTERPRETER(metaD _metadata){return _metadata.data;}
 
-float EVBEXP(int selection,float a,float b)
+double EVBEXP(int selection,double a,double b)
 {
 	switch(selection){
 		case 0: return a+b;
 		case 1: return a-b;
 		case 2: return a*b;
-		case 3: return (b==0)?(a/0.000001f):(a/b);
+		case 3: return (b==0)?(a/0.0000000000000000000000000000000000001f):(a/b);
 		case 4: return pow(a,b);
 		default : return 1.0f;
 	}
 	return 1.0f;
 }
-float EVUEXP(int selection,float a)
+double EVUEXP(int selection,double a)
 {
 	switch(selection){
 		case 0: return exp(a);
@@ -101,15 +107,15 @@ float EVUEXP(int selection,float a)
 
 int main(int argc, char const *argv[])
 {
-	_values =(float**)calloc(40,sizeof(int*));
-	for(int index = 0; index<40; index++)
+	srand((int)time(NULL));
+	for(int index = 0; index<samples; index++)
 	{
-		_values[index] = (float*)calloc(2,sizeof(float));
-		_values[index][0] = index/20.0f+1.0f;
-		_values[index][1] = f(_values[index][0]);
+		double* temp_vect= (double*)calloc(2,sizeof(double));
+		temp_vect[0] = index+2.0f;
+		temp_vect[1] = f(temp_vect[0])+(double)(rand()%1000000/10000000.0f);
+		_values.push_back(temp_vect);
 	}
 
-	srand((int)time(NULL));
 	Node<metaD> *myNode1;
 	Node<metaD> *myNode2;
 	Node<metaD> *subnode1=new Node<metaD>();
@@ -121,45 +127,45 @@ int main(int argc, char const *argv[])
 	int half_nodes2=0;
 
 	printf("...\n");
-	myNode1=newNode(HALF_HALF,DEPTH,GETOP(),ISBIN,&GETLEAF,&GETOP);
-	myNode2=newNode(HALF_HALF,DEPTH,GETOP(),ISBIN,&GETLEAF,&GETOP);
+	myNode1=newNode(HALF_FILL,DEPTH,GETO(),IBN,&GETL,&GETO);
+	myNode2=newNode(HALF_FILL,DEPTH,GETO(),IBN,&GETL,&GETO);
 
-	PrintPosOrder( myNode1, ISNODE , ISBIN , GETCHARREP );
+	PrintTree( myNode1, IND , IBN , GCHR );
 	printf("\n\n");
-	PrintPosOrder( myNode2, ISNODE , ISBIN , GETCHARREP );
+	PrintTree( myNode2, IND , IBN , GCHR );
 	printf("\n");
 
-	printf("\n evaluation_result1: %f\n",evalFunction(myNode1,EVBEXP,EVUEXP,ISNODE,ISBIN,ISVAR,GETVARINDEX,GETTERMINAL,GETEXPINDEX,_values[0]));
-	printf("\n cuadraticError1: %f\n",cuadraticError(myNode1,EVBEXP,EVUEXP,ISNODE,ISBIN,ISVAR,GETVARINDEX,GETTERMINAL,GETEXPINDEX,_values,VARIABLE_SIZE,(size_t)sizeof(_values)/sizeof(_values[0])));
+	printf("\n evaluation_result1: %f\n",evalFunction(myNode1,EVBEXP,EVUEXP,IND,IBN,IVR,GVI,GT,GEI,_values[0]));
+	printf("\n cuadraticError1: %f\n",cuadraticError(myNode1,EVBEXP,EVUEXP,IND,IBN,IVR,GVI,GT,GEI,_values,(int)_values.size(),varRep.size()));
 
-	printf("\n evaluation_result2: %f\n",evalFunction(myNode2,EVBEXP,EVUEXP,ISNODE,ISBIN,ISVAR,GETVARINDEX,GETTERMINAL,GETEXPINDEX,_values[0]));
-	printf("\n cuadraticError2: %f\n",cuadraticError(myNode2,EVBEXP,EVUEXP,ISNODE,ISBIN,ISVAR,GETVARINDEX,GETTERMINAL,GETEXPINDEX,_values,VARIABLE_SIZE,(size_t)sizeof(_values)/sizeof(_values[0])));
+	printf("\n evaluation_result2: %f\n",evalFunction(myNode2,EVBEXP,EVUEXP,IND,IBN,IVR,GVI,GT,GEI,_values[0]));
+	printf("\n cuadraticError2: %f\n",cuadraticError(myNode2,EVBEXP,EVUEXP,IND,IBN,IVR,GVI,GT,GEI,_values,(int)_values.size(),varRep.size()));
 
-	nodeCount1 = nodeCounter(myNode1,ISNODE,ISBIN);
-	nodeCount2 = nodeCounter(myNode2,ISNODE,ISBIN);
+	nodeCount1 = nodeCounter(myNode1,IND,IBN);
+	nodeCount2 = nodeCounter(myNode2,IND,IBN);
 
-	half_nodes1 = (int)ceil(nodeCount1/2);
-	half_nodes2 = (int)ceil(nodeCount2/2);
+	half_nodes1 = (int)ceil(nodeCount1*PROP);
+	half_nodes2 = (int)ceil(nodeCount2*PROP);
 
 	printf("number of nodes 1: %d, half_ceiled: %d\n",nodeCount1,half_nodes1);
 	printf("number of nodes 2: %d, half_ceiled: %d\n",nodeCount2,half_nodes2);
 
-	getIndexedSubTree(half_nodes1,myNode1,subnode1,ISNODE,ISBIN);
-	getIndexedSubTree(half_nodes2,myNode2,subnode2,ISNODE,ISBIN);
+	getIndexedSubTree(half_nodes1,myNode1,subnode1,IND,IBN);
+	getIndexedSubTree(half_nodes2,myNode2,subnode2,IND,IBN);
 
 	printf("selected subtree1: \n");
-	PrintPosOrder( subnode1,ISNODE,ISBIN,GETCHARREP);
+	PrintTree( subnode1,IND,IBN,GCHR);
 	printf("\n");
 	printf("selected subtree2: \n");
-	PrintPosOrder( subnode2,ISNODE,ISBIN,GETCHARREP);
+	PrintTree( subnode2,IND,IBN,GCHR);
 
 	printf("\nsubstitution...\n");
-	setIndexedSubTree(half_nodes1,myNode1,subnode2,ISNODE,ISBIN);
-	setIndexedSubTree(half_nodes2,myNode2,subnode1,ISNODE,ISBIN);
+	setIndexedSubTree(half_nodes1,myNode1,subnode2,IND,IBN);
+	setIndexedSubTree(half_nodes2,myNode2,subnode1,IND,IBN);
 
-	PrintPosOrder(myNode1,ISNODE,ISBIN,GETCHARREP);
+	PrintTree(myNode1,IND,IBN,GCHR);
 	printf("\n");
-	PrintPosOrder(myNode2,ISNODE,ISBIN,GETCHARREP);
-	
+	PrintTree(myNode2,IND,IBN,GCHR);
+	printf("\n");
 	return 0;
 }
